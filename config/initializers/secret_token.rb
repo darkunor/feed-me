4b43f8549b28141ab5cd401156ed15117f6d08a9
Habib Alamin <ha.alamin@gmail.com>
2014-09-18 15:50:01 +0100

    "rails new feed_me -d postgresql".

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FeedMe::Application.config.secret_token = '3526aa40a8ebaa304017c9c12a450f24ae7b3632ac55e12ef20bc8c12cfc1d9d79c55fcf28e19d6886d7272234c492107fa348bf2e30b00c72487b19a670c803'
