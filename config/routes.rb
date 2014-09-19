745d9e0739badfdd57f191a3d6e96d2bf78c604d
Habib Alamin <ha.alamin@gmail.com>
2014-09-19 12:15:30 +0100

    "rails g devise User", then "rake db:migrate".

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 FeedMe::Application.routes.draw do
+  devise_for :users
+
   root to: 'subscriptions#index'
   # The priority is based upon order of creation:
   # first created -> highest priority.
